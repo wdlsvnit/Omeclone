d7f218fae63c2ff44abe9956a14cbfc9c9e55029
Sumedh Nimkarde <blesssumedh@gmail.com>
2018-01-15 20:13:54 +0530

    Add socket server

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8000;
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+  console.log(`Socket: ${socket.id}`);
+  socket.emit('ack', { id: socket.id });
+});
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -14,5 +22,6 @@ app.get('/chat', (req, res) => {
   res.render('chat');
 });
 
-app.listen(port);
-console.log(`Server is runnning at ${port}`);
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
