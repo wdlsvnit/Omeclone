443fd48729d77ae51f43487ba5dc51e95cd75757
Rohit Ramesh <rohitramesh47@gmail.com>
2018-01-15 15:17:14 +0530

    Add chat page route in index.js

--- index.js
+++ index.js
@@ -10,5 +10,9 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/chat', (req, res) => {
+	res.render('chat');
+});
+
 app.listen(8000);
 console.log(`Server is runnning at ${port}`);
