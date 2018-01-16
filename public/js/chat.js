0161239f89ae59d626e03ef67d1041eddd8a6a24
Sumedh Nimkarde <blesssumedh@gmail.com>
2018-01-16 21:44:21 +0530

    Correct endpoint for socket server

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,7 @@
+'use strict';
+
 (function () {
-  let socket = io.connect('http://localhost:3000');
+  let socket = io.connect(`${window.location.hostname}:${window.location.port}`);
   socket.on('ack', (d) => {
     console.log(`Received: ${d}`);
   });
