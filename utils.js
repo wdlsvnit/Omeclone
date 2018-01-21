c04f7dd2fda55a3cbadd90f6e9127c128dd3bd47
Sumedh Nimkarde <blesssumedh@gmail.com>
2018-01-21 14:30:23 +0530

    Make the prototype of bidirectional messaging

--- utils.js
+++ utils.js
@@ -15,7 +15,7 @@ function makeRoom(queue) {
   // Return a room object from here which we will add to our global rooms. The object format should be like:
   return {
     "roomID": 234234, // A random number which should be unique for every room. Figure out the logic. You can use any external npm module.
-    "users" : [] // This will be an array containing the first objefct from the queue above and a object at random index in queue apart from first one.
+    "users" : [queue[0], queue[1]] // This will be an array containing the first objefct from the queue above and a object at random index in queue apart from first one.
   }
 }
 
