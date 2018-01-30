a7c130ebe2075b778c53843b2e27b2830f7caa15
Sumedh Nimkarde <blesssumedh@gmail.com>
2018-01-30 14:24:39 +0530

    Enable private chatroom messaging

--- socketserver.js
+++ socketserver.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const utils = require('./utils.js');
-// console.log(utils.makeUserObject);
+const uniqueID = require('uniqid');
 
 // This is our socket server. All the events socket events will go here.
 // Export the socket server
@@ -13,8 +13,40 @@ module.exports = (io, app) => {
 
     onlineUsers.push(socket);
     socket.on('privateRoom', (user) => {
-      socket.join(user.room);
-      socket.emit('private ack', { "message": "Added to privateRoom" });
+      let unfilledRooms = rooms.map((room) => {
+        if (!room.isFilled) {
+          console.log(room.roomID);
+          return room;
+        }
+      });
+      console.log(`Unfilled Rooms: ${JSON.stringify(unfilledRooms[0])}`);
+      try {
+        // join the existing room.
+        socket.join(unfilledRooms[0].roomID);
+        unfilledRooms[0].isFilled = true;
+        socket.emit('private ack', { "message": "Added to privateRoom", "roomID": unfilledRooms[0].roomID });
+        console.log(`Joined existing room: ${unfilledRooms[0].roomID}`);
+        console.log(`--------------------------------------------`);
+      }
+      catch(e) {
+        // dont have unfilled rooms. Thus creating a new user.
+        let uID = uniqueID();
+        console.log(`Created new room: ${uID}`);
+        rooms.push({ "roomID": uID, "isFilled": false });
+        socket.join(uID);
+        console.log(`Socket joined in room with id: ${uID}`);
+        socket.emit('private ack', { "message": "Added to privateRoom", "roomID": uID });
+        // console.log(`Current status of rooms: ${rooms}`);
+        console.log(`--------------------------------------------`);
+      }
+      // create an unique id here.
+      // let uID = uniqueID();
+      // console.log(uID);
+      // rooms.push({ "roomID": uID, "isFilled": false });
+      // // Maintain a global room array which would store the room ids.
+      // socket.join(uID);
+      // // emit the room id to the frontend side.
+      // socket.emit('private ack', { "message": "Added to privateRoom", "roomID": uID });
     });
 
     socket.on('sendMessage', (data) => {
