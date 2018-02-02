207feb3c82d13e8b6a949c6ef4a3aa1b70a17499
Unknown <avinashjaiswal642@gmail.com>
2018-02-03 04:37:34 +0530

    Create new index page

---
+++ public/js/parallax.js
@@ -0,0 +1,6 @@
+var scene1 = document.getElementById('scene1');
+var parallaxInstance1 = new Parallax(scene1,{
+});
+var scene2 = document.getElementById('scene2');
+var parallaxInstance2 = new Parallax(scene2,{
+});
