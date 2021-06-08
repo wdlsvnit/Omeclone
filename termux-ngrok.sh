57ee352428d5ce4d1b6ea660c106179edc6dc97d
EnmentGit <84762469+EnmentGit@users.noreply.github.com>
2021-06-08 12:41:41 +0300

    Update termux-ngrok.sh

--- termux-ngrok.sh
+++ termux-ngrok.sh
@@ -4,23 +4,14 @@ download() {
     wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
     unzip ngrok-stable-linux-amd64.zip
     rm -rf ngrok-stable-linux-amd64.zip
-    echo "Download complete" 
+    cd ngrok-stable-linux-amd64
+    ./ngrok authtoken 1oWDFci0TDVwcGZkeeLf4sJsmjn_4xgJcnqbj5BgVetRV19uz
+    ./ngrok http 3000
+    echo "ngrok complete" 
 }
 
 # start web server
 #echo "Starting web server..."
 
-# start tunnel
-echo "Starting ngrok tunnel in region ap"
-cd ngrok-stable-linux-amd64
-./ngrok authtoken 1oWDFci0TDVwcGZkeeLf4sJsmjn_4xgJcnqbj5BgVetRV19uz
-./ngrok tcp -region ap --log=stdout 1025 > | sed '/started tunnel/ q'
-orig_server_ip=`curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url'`
-trimmed_server_ip=`echo $orig_server_ip | grep -o '[a-zA-Z0-9.]*\.ngrok.io[0-9:]*'`
-server_ip="${trimmed_server_ip:-$orig_server_ip}"
-echo "Server IP is: $server_ip"
-echo "Server running on: $server_ip" >
-
 # Start web
-#PATH=$PWD/jre/bin:$PATH
 echo "Running server..."
