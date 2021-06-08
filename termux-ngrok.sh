2bfdc536fd41b73e333f01d15869252d9d58beaf
EnmentGit <84762469+EnmentGit@users.noreply.github.com>
2021-06-08 15:30:22 +0300

    Update termux-ngrok.sh

--- termux-ngrok.sh
+++ termux-ngrok.sh
@@ -1,9 +1,12 @@
-node index.js
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok.zip
 rm -rf ngrok.zip
 ./ngrok authtoken 1tf5A0O0WRoF3XHGwnSdrazDWfa_hUSqFBb6GMJo1gpLEWrx
+node index.js
 ./ngrok http 3000
+netlify deploy
+
+
 
 
 
