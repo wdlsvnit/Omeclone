c5f86e0dfae8c851bab84433d446b972fb8d7e52
EnmentGit <84762469+EnmentGit@users.noreply.github.com>
2021-06-08 11:33:21 +0300

    Update termux-ngrok.sh

--- termux-ngrok.sh
+++ termux-ngrok.sh
@@ -1,64 +1,45 @@
-#!/bin/dash
-# Desenvolvido por Tchelo Noob
-atualizar='\033[1;32m'
-echo "$atualizar Atualizando repositório..."
-apt update && upgrade -y
-apt install -y wget
-clear
-k='\033[01;32m'
-echo
-echo "${k}████████╗ ██████╗██╗  ██╗███████╗██╗      ██████╗ ";
-echo "${k}╚══██╔══╝██╔════╝██║  ██║██╔════╝██║     ██╔═══██╗";
-echo "${k}   ██║   ██║     ███████║█████╗  ██║     ██║   ██║";
-echo "${k}   ██║   ██║     ██╔══██║██╔══╝  ██║     ██║   ██║";
-echo "${k}   ██║   ╚██████╗██║  ██║███████╗███████╗╚██████╔╝";
-echo "${k}   ╚═╝    ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝ ";
-echo "${k} /_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/";
-echo
-echo "Deseja instalar o Ngrok? [Y/n]"
-read opcao
-case $opcao in
-y)
-echo
-echo "Downloading Termux-ngrok..."
-case `dpkg --print-architecture` in
-aarch64)
-    architectureURL="arm64" ;;
-arm)
-    architectureURL="arm" ;;
-armhf)
-    architectureURL="armhf" ;;
-amd64)
-    architectureURL="amd64" ;;
-i*86)
-    architectureURL="i386" ;;
-x86_64)
-    architectureURL="amd64" ;;
-*)
-    echo "Arquitetura desconhecida"
-esac
-
-wget "https://github.com/tchelospy/NgrokTest/blob/master/ngrok-stable-linux-${architectureURL}.zip?raw=true" -O ngrok.zip
-unzip ngrok.zip
-cat ngrok > /data/data/com.termux/files/usr/bin/ngrok
-chmod 700 /data/data/com.termux/files/usr/bin/ngrok
-rm ngrok ngrok.zip
-clear
-echo "${k}███╗   ██╗ ██████╗ ██████╗  ██████╗ ██╗  ██╗";
-echo "${k}████╗  ██║██╔════╝ ██╔══██╗██╔═══██╗██║ ██╔╝";
-echo "${k}██╔██╗ ██║██║  ███╗██████╔╝██║   ██║█████╔╝ ";
-echo "${k}██║╚██╗██║██║   ██║██╔══██╗██║   ██║██╔═██╗ ";
-echo "${k}██║ ╚████║╚██████╔╝██║  ██║╚██████╔╝██║  ██╗";
-echo "${k}╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝";
-echo
-echo "Exemplo de comando (ngrok http 80\nOu ngrok para ajuda)"
-;;
-
-n)
-clear
-echo "Ngrok não instalado :("
-echo
-esac
+#!/bin/bash
 
 
+require_executable "ngrok"
 
+# environment variables
+require_env "ngrok_token" "your ngrok authtoken from https://dashboard.ngrok.com"
+require_env "ngrok_region" "your region, one of:
+us - United States (Ohio)
+eu - Europe (Frankfurt)
+ap - Asia/Pacific (Singapore)
+au - Australia (Sydney)
+sa - South America (Sao Paulo)
+jp - Japan (Tokyo)
+in - India (Mumbai)" 
+
+# start web server
+#echo "Starting web server..."
+echo "Minecraft server starting, please wait" > $root/ip.txt
+
+# start tunnel
+mkdir -p ./logs
+touch ./logs/temp # avoid "no such file or directory"
+rm ./logs/*
+echo "Starting ngrok tunnel in region ap"
+./ngrok authtoken 1oWDFci0TDVwcGZkeeLf4sJsmjn_4xgJcnqbj5BgVetRV19uz
+touch logs/ngrok.log
+./ngrok tcp -region ap --log=stdout 1025 > ./logs/ngrok.log &
+# wait for started tunnel message, and print each line of file as it is written
+tail -f ./logs/ngrok.log | sed '/started tunnel/ q'
+orig_server_ip=`curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url'`
+trimmed_server_ip=`echo $orig_server_ip | grep -o '[a-zA-Z0-9.]*\.ngrok.io[0-9:]*'`
+server_ip="${trimmed_server_ip:-$orig_server_ip}"
+echo "Server IP is: $server_ip"
+echo "Server running on: $server_ip" > $root/ip.txt
+
+touch logs/latest.log
+# Experiment: Run http server after all ports are opened
+#( tail -f ./logs/latest.log | sed '/RCON running on/ q' && python3 -m http.server 8080 ) &
+
+# Start web
+#PATH=$PWD/jre/bin:$PATH
+echo "Running server..."
+.node index
+echo "Exit code $?"
