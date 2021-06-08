f8e2d7afc9b14aa8fcab78d750d2ed1a93b9ed91
EnmentGit <84762469+EnmentGit@users.noreply.github.com>
2021-06-08 14:21:58 +0300

    Update termux-ngrok.sh

--- termux-ngrok.sh
+++ termux-ngrok.sh
@@ -2,7 +2,7 @@ set -e
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok.zip
 rm -rf ngrok.zip
-./ngrok authtoken 1tf5A0O0WRoF3XHGwnSdrazDWfa_hUSqFBb6GMJo1gpLEWrx
+./ngrok authtoken 1Vnz8cfEaywc5xGAm02QrnSNfvS_5ZEZQuGzWaETeQi3gY4A2
 ./ngrok http 3000
 
 
