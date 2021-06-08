0492d9af4ad056ea2f15f951eeab4ad1460cddb4
EnmentGit <84762469+EnmentGit@users.noreply.github.com>
2021-06-08 12:08:55 +0300

    Update termux-ngrok.sh

--- termux-ngrok.sh
+++ termux-ngrok.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
+# Minecraft version
+VERSION=1.16.3
+
+set -e
+root=$PWD
+
+export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+download() {
+    set -e
+    wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+    unzip ngrok.zip
+    rm -rf ngrok.zip
+    echo "Download complete" 
+}
+
+require() {
+    if [ ! $1 $2 ]; then
+        echo $3
+        echo "Running download..."
+        download
+    fi
+}
+
+# ngrok binary
+require_executable "ngrok"
+
 # environment variables
-require_env "ngrok_token" "your ngrok authtoken from https://dashboard.ngrok.com"
-require_env "ngrok_region" "your region, one of:
+require_env "ngrok_token" "1oWDFci0TDVwcGZkeeLf4sJsmjn_4xgJcnqbj5BgVetRV19uz"
+require_env "ngrok_region" "ap"
 us - United States (Ohio)
 eu - Europe (Frankfurt)
 ap - Asia/Pacific (Singapore)
@@ -19,24 +47,20 @@ echo "Minecraft server starting, please wait" > $root/ip.txt
 mkdir -p ./logs
 touch ./logs/temp # avoid "no such file or directory"
 rm ./logs/*
-echo "Starting ngrok tunnel in region ap"
-./ngrok authtoken 1oWDFci0TDVwcGZkeeLf4sJsmjn_4xgJcnqbj5BgVetRV19uz
+echo "Starting ngrok tunnel in region $ngrok_region"
+./ngrok authtoken $ngrok_token
 touch logs/ngrok.log
-./ngrok tcp -region ap --log=stdout 1025 > ./logs/ngrok.log &
+./ngrok tcp -region $ngrok_region --log=stdout 1025 > ./logs/ngrok.log &
 # wait for started tunnel message, and print each line of file as it is written
 tail -f ./logs/ngrok.log | sed '/started tunnel/ q'
 orig_server_ip=`curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url'`
 trimmed_server_ip=`echo $orig_server_ip | grep -o '[a-zA-Z0-9.]*\.ngrok.io[0-9:]*'`
 server_ip="${trimmed_server_ip:-$orig_server_ip}"
 echo "Server IP is: $server_ip"
-echo "Server running on: $server_ip" > $root/ip.txt
-
-touch logs/latest.log
-# Experiment: Run http server after all ports are opened
-#( tail -f ./logs/latest.log | sed '/RCON running on/ q' && python3 -m http.server 8080 ) &
+echo "Server running on: $server_ip" >
 
 # Start web
 #PATH=$PWD/jre/bin:$PATH
 echo "Running server..."
 .node index
-echo "Exit code $?"
+
